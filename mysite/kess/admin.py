0a082643299e82b04e86d6666f2d9249a6e9f8c5
Chelpot <lucas.baquer@gmail.com>
2021-09-29 23:56:05 +0200

    Add category to kess

--- mysite/kess/admin.py
+++ mysite/kess/admin.py
@@ -4,6 +4,6 @@ from .models import Kess
 
 
 class KessAdmin(admin.ModelAdmin):
-    fields = ['date', 'emoji', 'reponse', 'isStaff']
-    list_display = ('reponse', 'emoji', 'date')
+    fields = ['date', 'emoji', 'reponse', 'isStaff', 'category']
+    list_display = ('reponse', 'emoji', 'date', 'category')
 admin.site.register(Kess, KessAdmin)
--- mysite/kess/models.py
+++ mysite/kess/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from enum import Enum
 
+class CategoryChoice(Enum):
+    DIVERS = "Divers"
+    GAMES = "Jeux vidéo"
+    MOVIE = "Films et séries"
+    EXPRESSION = "Expressions"
+    CULINARY = "Alimentation"
+    SPORT = "Sport"
+    CELEBRITY = "Célébrités et personnages "
 
 class Kess(models.Model):
     emoji = models.CharField(max_length=200)
     reponse = models.CharField(max_length=200)
-    isStaff = models.BooleanField()
+    isStaff = models.BooleanField(default=False)
     date = models.DateTimeField('date published')
+    category = models.CharField(
+        max_length=200,
+        choices=[(tag.value, tag.value) for tag in CategoryChoice],
+        default=CategoryChoice.DIVERS
+    )
+
+    def __str__(self):
+        return self.emoji
 
 
-def __str__(self):
-    return self.emoji
