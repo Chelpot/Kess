d1c3a919de3c936cd8ef3f7537c929e65ff945a1
Chelpot <lucas.baquer@gmail.com>
2021-10-04 00:25:16 +0200

    Add_kess view added / Time Zone changed

--- mysite/kess/urls.py
+++ mysite/kess/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'kess'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_kess/', views.add_kess, name='add_kess'),
     path('<int:kess_id>/', views.detail, name='detail'),
 ]
