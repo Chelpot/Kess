d358462454c5de151d23fffeab87b5117b2c7cbb
Chelpot <lucas.baquer@gmail.com>
2021-10-15 00:17:34 +0200

    rollbacktest

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -128,6 +127,8 @@ USE_TZ = True
 
 # All settings common to all environments
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -145,14 +146,4 @@ SESSION_COOKIE_SECURE = env('SESSION_COOKIE_SECURE')=='True'
 
 SECURE_SSL_REDIRECT = False
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# django_heroku.settings(locals())
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'),]
