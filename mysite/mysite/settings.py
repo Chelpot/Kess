b58b8de353c963b03404ab4566fae95d77fd7f05
Chelpot <lucas.baquer@gmail.com>
2021-10-15 00:10:33 +0200

    f

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -148,7 +148,7 @@ SECURE_SSL_REDIRECT = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/mysite/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
