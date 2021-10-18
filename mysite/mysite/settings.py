839a98e2a179c38bce9d613b3ec6a5e3e131c89b
Chelpot <lucas.baquer@gmail.com>
2021-10-18 08:21:38 +0200

    another try

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')=='True'
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'https://kess-finder-deploy.herokuapp.com/']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'kess-finder-deploy.herokuapp.com/', 'kess-finder-deploy.herokuapp.com/kess/']
 
 # Application definition
 
